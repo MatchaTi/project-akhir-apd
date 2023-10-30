dd8dd4b3e597df029ce9694e3f76e0d0c4938380
MatchaTi <tehmatcha28@gmail.com>
2023-10-30 20:47:30 +0800

    feat : menambahkan program layanan untuk user

---
+++ program.py
@@ -0,0 +1,14 @@
+def bmi():
+    print("Kalkulator BMI")
+
+
+def kalori():
+    print("Kalkulator Kalori")
+
+
+def hidrasi():
+    print("Kalkulator Hidrasi")
+
+
+def kebahagiaan():
+    print("Kalkulator Kebahagiaan")
