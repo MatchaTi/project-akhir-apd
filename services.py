cc307c07a3fb09b24fc4f02ede7df95e0f0fbfa2
MatchaTi <tehmatcha28@gmail.com>
2023-10-30 20:23:13 +0800

    feat : menambahkan services

---
+++ services.py
@@ -0,0 +1,21 @@
+import os
+from crud import data_users
+
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def enter_continue():
+    input("\nKlik enter untuk lanjut...")
+
+
+def check_role(role):
+    if role == "admin":
+        return True
+
+    return False
+
+
+def get_len_data():
+    return len(data_users[1:])
