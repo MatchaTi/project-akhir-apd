55cb665d3831164cbb7f99056e9b82c46a7fa7e8
MatchaTi <tehmatcha28@gmail.com>
2023-10-30 16:45:14 +0800

    chore : menambahkan menu pada main

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello")
+import os
+
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def main():
+    while True:
+        clear_screen()
+        print("Selamat Datang di Aplikasi Screening Kesehatan")
+        print("Menu:")
+        print("[1] Register")
+        print("[2] Login")
+        print("[3] Keluar")
+
+        pilihan = input("Pilih menu\t:")
+        if pilihan == "1":
+            print("Register")
+        elif pilihan == "2":
+            print("Login")
+        elif pilihan == "3":
+            return
+
+
+if __name__ == "__main__":
+    main()
+    clear_screen()
+    print("Program Selesai!")
